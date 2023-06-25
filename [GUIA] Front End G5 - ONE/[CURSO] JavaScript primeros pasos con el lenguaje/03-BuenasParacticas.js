b386b42a593808268d15c81718324d6246eea486
sebastianXV <69022009+sebastianXV@users.noreply.github.com>
2023-06-25 18:13:43 -0500

    uploading new exercises

---
+++ [GUIA] Front End G5 - ONE/[CURSO] JavaScript primeros pasos con el lenguaje/03-BuenasParacticas.js
@@ -0,0 +1,11 @@
+//son distintas variables
+let nombreEmpleado = "Jose Perez";
+let NombreEmpleado = "Jose";
+
+
+//se puede cambiar su tipo de valor
+let edadEmpleado = 40;
+edadEmpleado = "cuarenta";
+
+
+
