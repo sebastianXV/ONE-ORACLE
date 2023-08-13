f46261609d411b06ca43753c1793d7ad9ab751d0
sebastianXV <69022009+sebastianXV@users.noreply.github.com>
2023-08-13 17:57:02 -0500

    add react router DOM

--- [GUIA] React G5 - ONE/[CURSO] React Router Navegación en una SPA/pet-shop/src/App.js
+++ [GUIA] React G5 - ONE/[CURSO] React Router Navegación en una SPA/pet-shop/src/App.js
@@ -1,9 +1,22 @@
 import './assets/css/base/base.css';
 import './assets/css/componentes/card.css'
 
+import Home from "./pages/Home"
+import Page404 from './pages/Page404';
+import Sobre from "./pages/Sobre"
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 function App() {
+
   return (
     <>
+    <Router>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/sobre' element={<Sobre />} />
+          <Route path='*' element={<Page404 />} />
+        </Routes>
+    </Router>
 
     </>
   );
