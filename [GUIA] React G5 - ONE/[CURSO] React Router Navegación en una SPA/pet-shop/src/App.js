98bb4656e58a4dbf69d3867927e3e8775d2c6ee2
sebastianXV <69022009+sebastianXV@users.noreply.github.com>
2023-08-14 19:09:01 -0500

    finish course

--- [GUIA] React G5 - ONE/[CURSO] React Router Navegación en una SPA/pet-shop/src/App.js
+++ [GUIA] React G5 - ONE/[CURSO] React Router Navegación en una SPA/pet-shop/src/App.js
@@ -1,9 +1,13 @@
 import './assets/css/base/base.css';
 import './assets/css/componentes/card.css'
-
+import Header from './components/Header';
 import Home from "./pages/Home"
 import Page404 from './pages/Page404';
 import Sobre from "./pages/Sobre"
+import Post from './pages/Post';
+import Categoria from './pages/Categoria';
+
+
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
@@ -11,9 +15,12 @@ function App() {
   return (
     <>
     <Router>
+      <Header />
         <Routes>
           <Route path='/' element={<Home />} />
           <Route path='/sobre' element={<Sobre />} />
+          <Route path='/posts/:id' element={<Post/>} />
+          <Route path='/categoria/:id/*' element={<Categoria />} />
           <Route path='*' element={<Page404 />} />
         </Routes>
     </Router>
