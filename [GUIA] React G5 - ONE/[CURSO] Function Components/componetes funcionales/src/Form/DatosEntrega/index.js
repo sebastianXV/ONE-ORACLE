b5a974cbdc67c5232a8c53bc92e98fee50dd02a1
sebastianXV <69022009+sebastianXV@users.noreply.github.com>
2023-09-10 15:03:59 -0500

    aula 2

--- [GUIA] React G5 - ONE/[CURSO] Function Components/componetes funcionales/src/Form/DatosEntrega/index.js
+++ [GUIA] React G5 - ONE/[CURSO] Function Components/componetes funcionales/src/Form/DatosEntrega/index.js
@@ -1,7 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import { TextField, Button, Box } from "@mui/material";
+import { validarInput } from "./validacion";
+const DatosEntrega = ({ updateStep }) => {
 
-const DatosEntrega = () => {
+  const [address, setAddress] = useState({ value: '', valid: null })
+  const [city, setCity] = useState({ value: '', valid: null })
+  const [province, setProvince] = useState({ value: '', valid: null })
+
   return (
     <Box
       component="form"
@@ -12,6 +17,11 @@ const DatosEntrega = () => {
         justifyContent: "center",
         flexDirection: "column",
       }}
+      onSubmit={(e) => {
+        e.preventDefault()
+        updateStep(3)
+        console.log(address, city, province)
+      }}
     >
       <TextField
         label="Dirección"
@@ -19,6 +29,16 @@ const DatosEntrega = () => {
         fullWidth
         margin="dense"
         type="text"
+        value={address.value}
+        onChange={(input) => {
+          const value = input.target.value
+          const valid = validarInput(value)
+          setAddress({ value, valid })
+        }}
+        error={address.valid === false}
+        helperText={
+          address.valid === false && "Ingresa al menos 4 caracteres."
+        }
       />
       <TextField
         label="Ciudad"
@@ -26,6 +46,16 @@ const DatosEntrega = () => {
         fullWidth
         margin="dense"
         type="text"
+        value={city.value}
+        onChange={(input) => {
+          const value = input.target.value
+          const valid = validarInput(value)
+          setCity({ value, valid })
+        }}
+        error={city.valid === false}
+        helperText={
+          city.valid === false && "Ingresa al menos 4 caracteres."
+        }
       />
       <TextField
         label="Estado/Provincia"
@@ -33,6 +63,16 @@ const DatosEntrega = () => {
         fullWidth
         margin="dense"
         type="text"
+        value={province.value}
+        onChange={(input) => {
+          const value = input.target.value
+          const valid = validarInput(value)
+          setProvince({ value, valid })
+        }}
+        error={province.valid === false}
+        helperText={
+          province.valid === false && "Ingresa al menos 4 caracteres."
+        }
       />
       <Button variant="contained" type="submit">
         Crear cuenta
--- [GUIA] React G5 - ONE/[CURSO] Function Components/componetes funcionales/src/Form/DatosPersonales/index.js
+++ [GUIA] React G5 - ONE/[CURSO] Function Components/componetes funcionales/src/Form/DatosPersonales/index.js
@@ -1,7 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import { TextField, Button, Box } from "@mui/material";
+import { validarNombre, validarApellidos, validarTelefono } from "./validaciones"
+const DatosPersonales = ({ updateStep }) => {
 
-const DatosPersonales = () => {
+  const [name, setName] = useState({ value: '', valid: null })
+  const [lastName, setLastName] = useState({ value: '', valid: null })
+  const [phone, setPhone] = useState({ value: '', valid: null })
+
   return (
     <Box
       component="form"
@@ -12,6 +17,10 @@ const DatosPersonales = () => {
         justifyContent: "center",
         flexDirection: "column",
       }}
+      onSubmit={(e) => {
+        e.preventDefault()
+        updateStep(2)
+      }}
     >
       <TextField
         label="Nombre"
@@ -19,6 +28,16 @@ const DatosPersonales = () => {
         fullWidth
         margin="dense"
         type="text"
+        value={name.value}
+        onChange={(input) => {
+          const value = input.target.value
+          const valid = validarNombre(value)
+          setName({ value, valid })
+        }}
+        error={name.valid === false}
+        helperText={
+          name.valid === false && "Ingresa al menos 2 caracteres y maximo 30."
+        }
       />
       <TextField
         label="Apellidos"
@@ -26,6 +45,16 @@ const DatosPersonales = () => {
         fullWidth
         margin="dense"
         type="text"
+        value={lastName.value}
+        onChange={(input) => {
+          const value = input.target.value
+          const valid = validarApellidos(value)
+          setLastName({ value, valid })
+        }}
+        error={lastName.valid === false}
+        helperText={
+          lastName.valid === false && "Ingresa al menos 2 caracteres y maximo 50."
+        }
       />
       <TextField
         label="Número telefónico"
@@ -34,6 +63,16 @@ const DatosPersonales = () => {
         margin="dense"
         type="number"
         inputProps={{ inputMode: "numeric", pattern: "[0-9]*" }}
+        value={phone.value}
+        onChange={(input) => {
+          const value = input.target.value
+          const valid = validarTelefono(value)
+          setPhone({ value, valid })
+        }}
+        error={phone.valid === false}
+        helperText={
+          phone.valid === false && "Ingresa al menos 8 numeros y maximo 15."
+        }
       />
       <Button variant="contained" type="submit">
         Siguiente
